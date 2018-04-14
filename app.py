b3a25c8edd2e2e415d2391e9347a492cc2eae23e
Vernon Johnson <vtj2105@columbia.edu>
2018-04-14 17:51:53 -0400

    Adds routes and templates for viewing food listings and submitting entries

--- app.py
+++ app.py
@@ -3,10 +3,16 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
-
 def home():
 	return render_template("index.html")
 
+@app.route("/food_entry")
+def submit_food_entry():
+	return render_template("food_entry.html")
+
+@app.route("/food_listings")
+def food_listings():
+	return render_template("food_listings.html")
 
 if __name__ == "__main__":
 	app.run(debug=True)
